7e48ac2df08673c2bfb904b022d5c1f36dc03778
maryam-amn <Maryam.Aman@jobtrek.ch>
2024-12-20 11:04:43 +0100

    feat: completed the function in the arrays file

--- src/arrays/arrays.ts
+++ src/arrays/arrays.ts
@@ -12,6 +12,15 @@ export function removeNumbersBelowThreshold(
   threshold: number,
 ): number[] | [] {
   // Write your code here
+  let arr: number[] = [];
+
+  array.forEach((element) => {
+    if (element >= threshold) {
+      arr.push(element);
+    } else {
+    }
+  });
+  return arr;
 }
 
 export function filterStringByLength(
@@ -20,8 +29,17 @@ export function filterStringByLength(
   maxLength: number,
 ): string[] | [] {
   // Write your code here
+
+  const array2: string[] = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length <= maxLength && array[i].length >= minLength) {
+      array2.push(array[i]);
+    }
+  }
+  return array2;
 }
 
 export function getOnlyTheLowercaseLettersOfTheString(string: string): string {
   // Write your code here
+  return (string.match(/[a-z]/g) || []).join('');
 }
